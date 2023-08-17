90ec00ff9259afe9c586fa50a3410d18b9358066
AaqibqQureshi <Aaqibqureshi321@gmail.com>
2023-08-17 07:59:59 +0500

    project setup. added navigation, WelcomeScreen, SignInScreen

---
+++ src/constants/colors.js
@@ -0,0 +1,5 @@
+export const colors = {
+    PURPLE: '#CB98EB',
+    WHITE: '#FFFFFF',
+    GRAY: "#635A8F",
+};
---
+++ src/screens/SignInScreen/SignInScreen.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import {Image, ImageBackground, Text, View} from 'react-native';
+import styles from "./styles";
+import ButtonComponent from "../../components/ButtonComponent/ButtonComponent";
+
+export default function SignInScreen() {
+    return (
+        <View style={styles.container}>
+      <Text>SignInScreen</Text>
+        </View>
+    );
+}
+
