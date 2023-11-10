970bca3a3689f4199580a979c2d4fccfd31a09f2
AaqibqQureshi <Aaqibqureshi321@gmail.com>
2023-11-10 21:19:11 +0500

    Revert "project setup. added navigation, WelcomeScreen, SignInScreen"
    This reverts commit 20d0cf1c992fe3babbdf8fbc8c5577f311093506.

--- App.js
+++ App.js
@@ -1,25 +1,20 @@
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import WelcomeScreen from "./src/screens/WelcomeScreen/WelcomeScreen";
-import {AppNavigator} from "./src/navigation/AppNavigator";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    return (
-        // <NavigationContainer>
-            <View style={styles.container}>
-              <AppNavigator />
-                {/*<StatusBar style="auto" />*/}
-            </View>
-        // </NavigationContainer>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        // backgroundColor: '#fff',
-        // alignItems: 'center',
-        // justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
--- src/constants/colors.js
+++
@@ -1,5 +0,0 @@
-export const colors = {
-    PURPLE: '#CB98EB',
-    WHITE: '#FFFFFF',
-    GRAY: "#635A8F",
-};
